f484d61b34d999c5bde183d0528ca9871134f200
MaxWarren9 <maximvoinoff@gmail.com>
2025-12-06 13:09:50 +0300

    init

---
+++ src/main/java/com/sds/x6_order/X6OrderApplication.java
@@ -0,0 +1,13 @@
+package com.sds.x6_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X6OrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sds/x6_order/X6OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.x6_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
